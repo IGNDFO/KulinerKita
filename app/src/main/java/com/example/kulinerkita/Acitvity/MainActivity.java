a4699d477357a51151374886358e5d4dad93399b
fernando <fernand0753@mhs.mdp.ac.id>
2023-05-08 09:01:36 +0700

    update

--- app/src/main/java/com/example/kulinerkita/Acitvity/MainActivity.java
+++ app/src/main/java/com/example/kulinerkita/Acitvity/MainActivity.java
@@ -1,17 +1,84 @@
 package com.example.kulinerkita.Acitvity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.example.kulinerkita.API.APIRequestData;
+import com.example.kulinerkita.API.RetroServer;
+import com.example.kulinerkita.Adapter.adapterKuliner;
+import com.example.kulinerkita.Model.ModelKuliner;
+import com.example.kulinerkita.Model.ModelResponse;
 import com.example.kulinerkita.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+private RecyclerView rvkuliner;
+private FloatingActionButton fabtambah;
+private ProgressBar pbkuliner;
+
+private RecyclerView.Adapter ad_kuliner;
+private RecyclerView.LayoutManager lm_kulnier;
+private List<ModelKuliner> listkuliner=new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvkuliner = findViewById(R.id.rv_kuliner);
+        fabtambah = findViewById(R.id.fab_tambah);
+        pbkuliner = findViewById(R.id.pb_kuliner);
+
+        lm_kulnier = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rvkuliner.setLayoutManager(lm_kulnier);
+    }
+    protected void  onResume(){
+        super.onResume();
+        retrievekuliner();
+    }
+
+        public void retrievekuliner (){
+            pbkuliner.setVisibility(View.VISIBLE);
+
+            APIRequestData ARD= new RetroServer().konekretro().create(APIRequestData.class);
+            Call<ModelResponse> proses= ARD.ardRetrieve();
+            proses.enqueue(new Callback<ModelResponse>() {
+                @Override
+                public void onResponse(Call<ModelResponse> call, Response<ModelResponse> response) {
+                    String kode =response.body().getKode();
+                    String  pesan =response.body().getPesan();
+                    listkuliner= response.body().getData();
+
+                    ad_kuliner=new adapterKuliner(MainActivity.this,listkuliner);
+                    rvkuliner.setAdapter(ad_kuliner);
+                    ad_kuliner.notifyDataSetChanged();
+                    pbkuliner.setVisibility(View.GONE);
+
+
+                }
+
+                @Override
+                public void onFailure(Call<ModelResponse> call, Throwable t) {
+                    Toast.makeText(MainActivity.this, "GAGAL MEMANGGIL SEVER", Toast.LENGTH_SHORT).show();
+                    pbkuliner.setVisibility(View.GONE);
+
+                }
+            });
+
 
     }
 }
---
+++ app/src/main/java/com/example/kulinerkita/Adapter/adapterKuliner.java
@@ -0,0 +1,63 @@
+package com.example.kulinerkita.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.kulinerkita.Model.ModelKuliner;
+import com.example.kulinerkita.R;
+
+import java.util.List;
+
+public class adapterKuliner extends RecyclerView.Adapter<adapterKuliner.VHkuliner>{
+private Context ctx;
+private List<ModelKuliner> listkuliner;
+
+    public adapterKuliner(Context ctx, List<ModelKuliner> listkuliner) {
+        this.ctx = ctx;
+        this.listkuliner = listkuliner;
+    }
+
+    @NonNull
+    @Override
+    public adapterKuliner.VHkuliner onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View varview= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,parent,false);
+        return new VHkuliner(varview);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull adapterKuliner.VHkuliner holder, int position) {
+ModelKuliner mk=listkuliner.get(position);
+        holder.tvid.setText(mk.getId());
+        holder.tvnama.setText(mk.getNama());
+        holder.tvasal.setText(mk.getAsal());
+        holder.tvdeskripsisingkat.setText(mk.getDeskripsi_singkat());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    public class VHkuliner extends RecyclerView.ViewHolder{
+        TextView tvid,tvnama,tvasal,tvdeskripsisingkat;
+        public VHkuliner(@NonNull View itemView) {
+            super(itemView);
+
+            tvid=  itemView.findViewById(R.id.tv_ID);
+            tvnama=  itemView.findViewById(R.id.tv_nama);
+            tvasal=  itemView.findViewById(R.id.tv_asal);
+            tvdeskripsisingkat=  itemView.findViewById(R.id.tv_deskripsi_singkat);
+        }
+    }
+
+}
+
+
